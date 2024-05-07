2e8e598dec83931d793aae00680b330d0bd2e905
Timo <georgethirtynine@gmail.com>
2024-05-07 15:45:37 -0600

    Made players drop their inventory before being banned

--- src/main/java/com/tiptow/Main.java
+++ src/main/java/com/tiptow/Main.java
@@ -2,9 +2,11 @@ package com.tiptow;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
@@ -22,10 +24,28 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        String playerName = event.getEntity().getName();
+        Player player = event.getEntity();
+        String playerName = player.getName();
+
+        // Drop the player's items
+        for (ItemStack item : player.getInventory().getContents()) {
+            if (item != null) {
+                player.getWorld().dropItemNaturally(player.getLocation(), item);
+            }
+        }
+        for (ItemStack item : player.getInventory().getArmorContents()) {
+            if (item != null) {
+                player.getWorld().dropItemNaturally(player.getLocation(), item);
+            }
+        }
+
+        // Clear the player's inventory
+        player.getInventory().clear();
+
         // Ban the player with a custom message
         Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), "ban " + playerName + " You have died!");
     }
+
 
     // Override default death message
     @EventHandler
@@ -33,4 +53,3 @@ public class Main extends JavaPlugin implements Listener {
         event.setDeathMessage(ChatColor.DARK_RED + "☠ " + event.getEntity().getName() + " has died!");
     }
 }
-
